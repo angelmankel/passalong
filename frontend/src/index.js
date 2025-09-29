102452102ad5c5409f0b266c254f50a6a68cae5f
Angel Mankel <angelmankel@gmail.com>
2025-09-29 14:26:42 -0700

    fixed dark theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
+    <MantineProvider defaultColorScheme="dark" withGlobalStyles withNormalizeCSS>
       <ModalsProvider>
         <Notifications />
         <App />
